3b218cfe98e7d6f1379628e16b829234ed2c5f73
roynaldi19 <roynaldi19@gmail.com>
2022-09-22 23:22:15 +0700

    Latihan Mengunggah File Ke Server

---
+++ app/src/main/java/com/dicoding/picodiploma/mycamera/model/FileUploadResponse.kt
@@ -0,0 +1,12 @@
+package com.dicoding.picodiploma.mycamera.model
+
+import com.google.gson.annotations.SerializedName
+
+data class FileUploadResponse(
+
+	@field:SerializedName("error")
+	val error: Boolean? = null,
+
+	@field:SerializedName("message")
+	val message: String? = null
+)
